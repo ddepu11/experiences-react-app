bffbdb3cfa93e0c7776ad2b9c2f63607d8095d92
Deepanshu Tiwari <ddepu11@gmail.com>
2021-03-10 16:53:00 +0530

    grid applied

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [employeesData, setEmployeesData] = useState([]);
+  const [employees, setEmployees] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <h1>Experience</h1>
+        <div className="underline"></div>
       </header>
+
+      <aside className="flex">
+        <button>Tommy</button>
+        <button>Tommy</button>
+        <button>Tommy</button>
+      </aside>
+
+      <main>
+        <h1>Full stack developer</h1>
+
+        <span>Tommy</span>
+
+        <span className="duration">december 2014 - present</span>
+
+        <div>
+          <i class="fas fa-forward fa-2x"></i>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus,
+            quas cupiditate aliquam qui eaque atque beatae illum? Exercitationem
+            aut, tenetur asperiores veniam ad eius nemo ab quam necessitatibus
+            dolor alias.
+          </p>
+        </div>
+      </main>
+      <footer>
+        <button>More Info</button>
+      </footer>
     </div>
   );
 }
